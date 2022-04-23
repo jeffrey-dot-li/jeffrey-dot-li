2df77c6efb4360140ee874b671309f0feb6b51f1
Jeffrey Li <dr.cheddar@outlook.com>
2022-04-23 15:24:12 -0400

    fix git submodules??? plsss

--- theme/project-config.example.ts
+++
@@ -1,7 +0,0 @@
-import { googleFonts, localFonts } from './google-fonts';
-export const projectFonts
-= {
-  content: googleFonts.Quicksand,
-  banner: localFonts['Pesta Stencil'],
-  title: googleFonts.Oswald,
-} as const;
